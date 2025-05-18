482ef1daefd53e10f469787339cb7bfee76717f3
NezdanchikG <NezdanchikTHANOS@gmail.com>
2025-05-19 01:56:57 +0300

    Change

--- terraform-yandex/yc_vars.sh
+++ terraform-yandex/yc_vars.sh
@@ -2,12 +2,10 @@
 
 YC_BIN="$HOME/yandex-cloud/bin/yc"
 
-YC_TOKEN=$($YC_BIN iam create-token)
-YC_CLOUD_ID=$($YC_BIN config get cloud-id)
-YC_FOLDER_ID=$($YC_BIN config get folder-id)
+YC_TOKEN=$("$YC_BIN" iam create-token)
+YC_CLOUD_ID=$("$YC_BIN" config get cloud-id)
+YC_FOLDER_ID=$("$YC_BIN" config get folder-id)
 
-jq -n \
-  --arg token "$YC_TOKEN" \
-  --arg cloud_id "$YC_CLOUD_ID" \
-  --arg folder_id "$YC_FOLDER_ID" \
-  '{"token": $token, "cloud_id": $cloud_id, "folder_id": $folder_id}'
+cat <<EOF
+{"token":"$YC_TOKEN","cloud_id":"$YC_CLOUD_ID","folder_id":"$YC_FOLDER_ID"}
+EOF
